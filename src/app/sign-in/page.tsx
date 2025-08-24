36848409603055150a3c6d45c1b6184f77b50035
Zoheir Djennane <zoheir.djennane@adextechnology.dz>
2025-08-24 06:11:33 +0100

    fix

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react';
 import AuthPage from "@/components/AuthPage";
 
 export default function SignInPage() {
-    return <AuthPage/>
+    return (
+        <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Loading...</div>}>
+            <AuthPage />
+        </Suspense>
+    );
 }
 
